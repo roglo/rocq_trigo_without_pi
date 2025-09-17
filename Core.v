10563f27e07730c5db279a2fb8d1e0a39d2c680d
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-09-17 02:02:45 +0200

    Add Core.v

---
+++ Core.v
@@ -0,0 +1,7 @@
+Require Export AngleDef.
+Require Export Angle.
+Require Export Angle_order.
+Require Export TrigoWithoutPiExt.
+Require Export AngleAddLeMonoL.
+Require Export AngleDiv2.
+Require Export AngleTan.
