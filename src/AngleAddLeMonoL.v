15a02f22799e71bf158dc6e49caae809141f818d
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2026-01-18 06:36:25 +0100

    -

--- src/AngleAddLeMonoL.v
+++ src/AngleAddLeMonoL.v
@@ -702,6 +702,37 @@ apply (rngl_cos_add_nonneg_cos_add_nonneg _ _ α2); try easy.
     progress sin_cos_add_sub_straight_hyp T Hs31.
     rewrite angle_sub_sub_distr.
     progress sin_cos_add_sub_straight_goal T.
+    destruct (rngl_leb_dec 0 (rngl_cos α2)) as [Hzc2| Hzc2]. {
+      apply rngl_leb_le in Hzc2.
+      change_angle_opp α2.
+      progress sin_cos_opp_hyp T H12.
+      progress sin_cos_opp_hyp T Hzs2.
+      progress sin_cos_opp_hyp T Hs32.
+      progress sin_cos_opp_hyp T H23.
+      progress sin_cos_opp_hyp T Hzc2.
+      progress sin_cos_opp_goal T.
+      change_angle_opp α3.
+      progress sin_cos_opp_hyp T H23.
+      progress sin_cos_opp_hyp T Hs32.
+      progress sin_cos_opp_hyp T Hzs3.
+      progress sin_cos_opp_hyp T Hs31.
+      progress sin_cos_opp_goal T.
+      apply (rngl_opp_nonpos_nonneg Hop Hor) in Hs31.
+      rewrite <- angle_opp_add_distr.
+      rewrite rngl_cos_opp.
+      rewrite rngl_add_comm.
+      apply rngl_add_cos_nonneg_when_sin_nonneg; [ easy | easy | | ]. {
+        rewrite angle_add_assoc.
+        rewrite <- angle_add_sub_swap.
+        rewrite angle_sub_add.
+        apply rngl_lt_le_incl in Hzs1, Hzs2, Hzc1.
+        now apply rngl_sin_add_nonneg.
+      }
+      apply rngl_lt_le_incl in Hzs2, Hzs3.
+      apply rngl_cos_sub_nonneg; [ easy | easy | easy | ].
+      now apply (rngl_le_trans Hor _ (rngl_cos α2)).
+    }
+    apply (rngl_leb_gt_iff Hto) in Hzc2.
 ...
 *)
 
